c07d1185cee5f7194ea276210fe8c126fc5320f7
Fire3Duck <pauavellanedalopez@gmail.com>
2025-10-08 12:36:23 +0200

    Maas

--- Assets/Scripte/PlayerControler.cs
+++ Assets/Scripte/PlayerControler.cs
@@ -1,108 +1,158 @@
-using System;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    //Componentes
     private CharacterController _controller;
 
-    //Inputs
     private InputAction _moveAction;
-    private InputAction _jumpAction;
+
     private Vector2 _moveInput;
+    private InputAction _jumpAction;
+
+    private InputAction _lookAction;
+    private Vector2 _lookInput;
 
     [SerializeField] private float _movementSpeed = 5;
     [SerializeField] private float _jumpHeight = 2;
 
     [SerializeField] private float _smoothTime = 0.2f;
-    private float _turnSmoothVelocity;
 
-    //Gravedad
+    private float _turnSmoothVelcity;   
+
+    //gravedad
+
     [SerializeField] private float _gravity = -9.81f;
+   
     [SerializeField] private Vector3 _playerGravity;
 
-    //Ground Sensor
+    //groundsensor
+
     [SerializeField] Transform _sensor;
+
     [SerializeField] LayerMask _groundLayer;
+
     [SerializeField] float _sensorRadius;
 
+    private Transform _maincamera;
 
     void Awake()
     {
-        _controller = GetComponent<CharacterController>();
+        _controller = GetComponent <CharacterController>();
         _moveAction = InputSystem.actions["Move"];
         _jumpAction = InputSystem.actions["Jump"];
-    }
+        _lookAction = InputSystem.actions["Look"];
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+        _maincamera = Camera.main.transform;
+    }
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        MovimientoCutre();
+        _moveInput = _moveAction.ReadValue<Vector2>();
+        _lookInput = _lookAction.ReadValue<Vector2>();
 
-        if (_jumpAction.WasPressedThisFrame() && IsGrounded())
+       // MovimientoCutre();
+       //Movimiento2();
+       Movement();
+
+        if(_jumpAction.WasPressedThisFrame() && IsGrounded())
         {
             Jump();
         }
-
         Gravity();
+    }
+
+    void Movement()
+    {
+         Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
+
+        if(direction != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _maincamera.eulerAngles.y; // para que el personaje gire la cabeza
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelcity, _smoothTime);
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            _controller.Move(moveDirection.normalized * _movementSpeed * Time.deltaTime);
+        }
+    }
+
+    void Movimiento2()
+    {
+        Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
+
+        Ray ray = Camera.main.ScreenPointToRay(_lookInput);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            Vector3 playerforward = hit.point - transform.position;
+            playerforward.y = 0;
+            transform.forward = playerforward;
+        }
+
+        if(direction != Vector3.zero)
+        {
+            _controller.Move(direction.normalized * _movementSpeed * Time.deltaTime);
+        }
     }
 
     void MovimientoCutre()
     {
-        _moveInput = _moveAction.ReadValue<Vector2>();
+        
 
         Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
 
-        if (direction!= Vector3.zero)
+        if(direction != Vector3.zero)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; //Atan2: Nos pilla valores de x y Y, calcula el angulo, para el el personaje mire donde se mueve.
-            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _smoothTime);
-
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; // para que el personaje gire la cabeza
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelcity, _smoothTime);
             transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
 
-            _controller.Move(direction.normalized * _movementSpeed * Time.deltaTime); //La velocidad del personaje siempre sera igual con el Time.deltaTime, independientemente de los frames.
+            _controller.Move(direction.normalized * _movementSpeed * Time.deltaTime);
         }
     }
 
         
 
+    
     void Jump()
     {
         _playerGravity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
 
         _controller.Move(_playerGravity * Time.deltaTime);
     }
-
+    
     void Gravity()
     {
-        if (!IsGrounded())
+        if(!IsGrounded())
         {
             _playerGravity.y += _gravity * Time.deltaTime;
         }
-        else if (IsGrounded() && _playerGravity.y < -2)
+        else if(IsGrounded() && _playerGravity.y < -2)
         {
             _playerGravity.y = _gravity;
         }
+
         _controller.Move(_playerGravity * Time.deltaTime);
     }
+    
 
     bool IsGrounded()
     {
         return Physics.CheckSphere(_sensor.position, _sensorRadius, _groundLayer);
     }
 
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(_sensor.position, _sensorRadius);
     }
+    
 }
-
